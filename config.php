61fbf34045e0b861b15e1028db37cbe09a7178cb
GabrielRecalde <53660946+GabrielRecalde@users.noreply.github.com>
2019-08-03 17:18:26 -0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+	$conexao = mysqli_connect("localhost", "padrao", "", "escola_em_dia");
+	$host = "localhost";
+	$user = "padrao";
+	$password = "";
+	$database = "escola_em_dia";
+	$url="http://localhost/Escola/";
+?>
